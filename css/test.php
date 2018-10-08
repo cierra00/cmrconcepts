ef532b113c736b2b27d257f578e9b1e466052c7a
Cierra Ray <cierraray2@gmail.com>
2018-10-07 20:10:11 -0400

    Initial commit

---
+++ css/test.php
@@ -0,0 +1,5 @@
+<?php 
+echo '<h1>Cierra Ray</h1>';
+
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CMR Concepts</title>
+    <link rel="stylesheet" href="/sass/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div id="site-title"><?php echo '<h1>CMR Concepts</h1>'; ?></div>
+        <nav>
+            <ul>
+                
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Designs</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+
+    </header>
+    <div class="scroller">I am legend</div>
+    <main class="container">
+       
+        <section id="showcase"><h1>hi</h1></section>
+        <section id="content row"></section>
+        <section id="suport-info"></section>
+        <section id="right"></section>
+    </main>
+    <footer>
+    
+    </footer>
+</body>
+</html>
